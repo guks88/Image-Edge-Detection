b4d140bf9f0483375a336ad3345c6f5b3985006f
guks88 <guks_88@hotmail.com>
2016-10-13 17:56:17 +0200

    filter
    unit test on filters

--- C#/ImageEdgeDetection/ImageFilters.cs
+++ C#/ImageEdgeDetection/ImageFilters.cs
@@ -53,20 +53,18 @@ namespace ImageEdgeDetection
         //apply color filter at your own taste
         public static Bitmap ApplyFilter(Bitmap bmp, int alpha, int red, int blue, int green)
         {
-
+            if(alpha<1||red<1||blue<1|| green<1)
+                throw new ArgumentException();
             Bitmap temp = new Bitmap(bmp.Width, bmp.Height);
 
 
             for (int i = 0; i < bmp.Width; i++)
-            {
                 for (int x = 0; x < bmp.Height; x++)
-                {
-                    Color c = bmp.GetPixel(i, x);
-                    Color cLayer = Color.FromArgb(c.A / alpha, c.R / red, c.G / green, c.B / blue);
-                    temp.SetPixel(i, x, cLayer);
-                }
-
-            }
+                    {
+                        Color c = bmp.GetPixel(i, x);
+                        Color cLayer = Color.FromArgb(c.A / alpha, c.R / red, c.G / green, c.B / blue);
+                        temp.SetPixel(i, x, cLayer);
+                    }
             return temp;
         }
 
--- C#/UnitTestProject/ImageFilterTest.cs
+++ C#/UnitTestProject/ImageFilterTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Drawing;
+using ImageEdgeDetection;
 
 namespace UnitTestProject
 {
@@ -12,7 +13,34 @@ namespace UnitTestProject
         public void NullExceptionOnNullImage()
         {
             Bitmap nulled = null;
-            //ImageFilters.
+            ImageFilters.ApplyFilter(nulled, 1, 1, 1, 1);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidFilterMinusOne()
+        {
+            Bitmap image = getImage(Color.FromArgb(100, 140, 200, 60));
+            ImageFilters.ApplyFilter(image, -1, -1, -1, -1);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidFilterzero()
+        {
+            Bitmap image = getImage(Color.FromArgb(100, 140, 200, 60));
+            ImageFilters.ApplyFilter(image, 0, 0, 0, 0);
+        }
+        [TestMethod]
+        public void ValidFilter()
+        {
+            Color origine = Color.FromArgb(100, 140, 200, 60);
+            Color wanted = Color.FromArgb(100, 70, 50, 60);
+            Bitmap imageBase = getImage(origine);
+            Bitmap filtred = ImageFilters.ApplyFilter(imageBase, 1, 2, 1, 4);
+            for (int with = 0; with < filtred.Width; with++)
+                for (int height = 0; height < filtred.Height; height++)
+                    Assert.AreEqual(filtred.GetPixel(with, height),wanted);
+
+
         }
 
         private Bitmap getImage(Color wantedColor)
