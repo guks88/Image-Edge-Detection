fb36d03a9b41695b3371f19c95dba113bb9a4f64
guks88 <guks_88@hotmail.com>
2016-10-13 17:30:44 +0200

    init
    init

---
+++ C#/UnitTestProject/ImageFilterTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class ImageFilterTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void NullExceptionOnNullImage()
+        {
+            Bitmap nulled = null;
+            //ImageFilters.
+        }
+
+        private Bitmap getImage(Color wantedColor)
+        {
+            Bitmap bmp = new Bitmap(50, 50);
+            for (int with = 0; with < bmp.Width; with++)
+                for (int height = 0; height < bmp.Height; height++)
+                    bmp.SetPixel(with, height, wantedColor);
+            return bmp;
+        }
+    }
+}
